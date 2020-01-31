f173aaa84f756d632886537ac1eb35f76a4f718d
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-31 10:20:26 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,8 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
-      puts
+      puts age_appropriate_birthday
+    end
+  end
+    
 
