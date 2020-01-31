1222f85e167c8bb67b92a946d17589830e85a51e
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-31 10:20:12 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,11 +6,11 @@
 
 def happy_birthday(birthday_kids)
  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    putsage_appropriate_birthday
   end # add your code snippet here!
 end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age <= 12
-      puts 
+      puts
 
